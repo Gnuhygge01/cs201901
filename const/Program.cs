3813734515697614e4aa29ccdb0aaa6126d1d11a
Gnuhygge01 <admin@TA-829-03>
2019-01-15 14:14:03 +0100

    kage

---
+++ const/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1223;
+            // finder selv ud af variabel int i = 1223; .... ved at kigge på højre side af =
+
+
+
+
+            //cd +tab+tab
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
