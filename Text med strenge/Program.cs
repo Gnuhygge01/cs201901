636c5fdb43263888fdd670cd4774734dbda81359
Gnuhygge01 <admin@TA-829-03>
2019-01-15 12:29:56 +0100

    endnu et projekt  nu med strenge

---
+++ Text med strenge/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_med_strenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fornavn = "Mikkel";
+            String efternavn = "Cronberg";
+
+            String samletNavn = fornavn + " " + efternavn;
+           
+            Console.WriteLine(samletNavn);
+
+            string navnStort = samletNavn.ToUpper();
+
+            Console.WriteLine(navnStort);
+
+            string navnLille = samletNavn.ToLower();
+
+            Console.WriteLine(navnLille);
+
+            string del = samletNavn.Substring(7, 4);
+
+            Console.WriteLine(del);
+
+
+
+            Console.WriteLine(fornavn + "\r\n\t" + efternavn);
+
+            System.IO.File.WriteAllText("c:\\temp\\test.txt", samletNavn);
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Variabler2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variabler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            System.DateTime c = new DateTime();
+
+            Console.WriteLine(c);
+
+            System.DateTime d = DateTime.Now;
+
+
+            string sti = "c:\\temp\\test"
+            String sti = @"c:\temp\test.dk";
+            Console.WriteLine(sti);
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
