327ca233c930926c19d3112f13539ce07bf51e27
Henrik Knutsson <hkn.gnu@gmail.com>
2019-01-20 19:50:22 +0100

    modulus forklar

--- Flow_Break_Continue/Program.cs
+++ Flow_Break_Continue/Program.cs
@@ -14,16 +14,17 @@ namespace Flow_Break_Continue
 
             for (int i = 0; i < 19; i++)
             {
-                if (i % 3 != 0)
+                if (i % 3 != 0) //her laves modulus med 3 
                     continue;
 
-                if (i > 15)
+                if (i > 15) //her sørger koden for at der ikke tælles 18 med
                     break;
 
                 Console.WriteLine(i);
             }
+ 
+            // det er bare sjovt ;-)
 
-            // Hold console åben ved debug
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 Console.Write("Press any key to continue . . . ");
