7e9b396cda621e613a1f430eb46b15fd07b0cc33
Henrik Knutsson <hkn.gnu@gmail.com>
2019-01-20 20:03:08 +0100

    simple og alligevel ikke :-)

---
+++ Metoder simple/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metoder_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res); // 7
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2); // 78.5398
+            Udskriv("Dette er en test"); // Dette er en test
+
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+
+        static double BeregnMoms(double tal, double momsPct)
+        {
+            return tal * momsPct;
+        }
+
+        static int LægSammen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double BeregnAreal(double radius)
+        {
+            return (radius * radius) * 3.1415;
+        }
+
+        static void Udskriv(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+
+
+    }
+    
+}
