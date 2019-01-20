77f30c8a05385665e9272f71e95c4263f511e41b
Henrik Knutsson <hkn.gnu@gmail.com>
2019-01-20 19:46:40 +0100

    leget med break

---
+++ Flow_Break_Continue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow_Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i < 19; i++)
+            {
+                if (i % 3 != 0)
+                    continue;
+
+                if (i > 15)
+                    break;
+
+                Console.WriteLine(i);
+            }
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+
+
+
+
+
+
+            }
+        }
+    }
+}
