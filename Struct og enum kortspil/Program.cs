b9f5771beef51f2d9bdf212973c1ab9341efc272
Henrik Knutsson <hkn.gnu@gmail.com>
2019-01-20 18:27:51 +0100

    udført ekstra kort

--- Struct og enum kortspil/Program.cs
+++ Struct og enum kortspil/Program.cs
@@ -28,14 +28,20 @@ namespace Struct_og_enum_kortspil
         {
 
             SpilleKort s1 = new SpilleKort();
-            s1.Værdi = 2;
+            s1.Værdi = 3;
             s1.Kulør = Kulør.Spar;
             Console.WriteLine($"{s1.Kulør} {s1.Værdi}");
 
             SpilleKort s2 = new SpilleKort();
-            s2.Værdi = 10;
+            s2.Værdi = 9;
             s2.Kulør = Kulør.Hjerter;
             Console.WriteLine($"{s2.Kulør} {s2.Værdi}");
+
+            SpilleKort s3 = new SpilleKort();
+            s3.Værdi = 5;
+            s3.Kulør = Kulør.Klør;
+            Console.WriteLine($"{s3.Kulør} {s3.Værdi}");
+
 
             // Hold console åben ved debug
             if (System.Diagnostics.Debugger.IsAttached)
