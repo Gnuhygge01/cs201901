573cd9cd49f5e360ca3a3c36137d6c4bd197e65d
admin <admin@TA-828-03>
2019-01-21 13:38:57 +0100

    oprettet fejlfinding med beeeep

---
+++ Fejl/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PrøvIgen();
+            }
+            catch (Exception ex)
+            {
+                Console.Beep();
+                Console.WriteLine($"der er sket en fejl igen og Chefen er tilkaldt" + ex.Message);
+            }
+            if (System.Diagnostics.Debugger.IsAttached)
+
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("Indtast 1. tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast 2. tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int res = tal1 + tal2;
+            Console.WriteLine("resultatet er " + res);
+        }
+        static void PrøvIgen()
+        {
+            int prøvAntalGange = 3;
+            int antal = 0;
+            while (antal < prøvAntalGange)
+            {
+                try
+                {
+                    Run();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.Beep();
+                    Console.WriteLine("Der er sket en fejl for sidste gang, du skal nu gå op til kontoret for en medarbejdersamtale, Direktøren er informeret " +  ex.Message);
+                    antal++;
+                }
+            }
+
+
+
+        }
+    }
+}
--- Rekursivmetode get file/Program.cs
+++ Rekursivmetode get file/Program.cs
@@ -11,7 +11,7 @@ namespace Rekursivmetode_get_file
         static void Main(string[] args)
         {
 
-            PrintFiler(@"c:\git");
+            
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
