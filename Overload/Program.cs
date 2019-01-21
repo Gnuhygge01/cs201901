c3798a6b83a4c817b562bac539e329c373eff79d
admin <admin@TA-828-03>
2019-01-21 11:36:16 +0100

    beregn opgave

--- Overload/Program.cs
+++ Overload/Program.cs
@@ -11,7 +11,38 @@ namespace Overload
         static void Main(string[] args)
         {
 
+            Console.WriteLine(Beregn(1, 2));
+
+            Console.WriteLine(Beregn(1,2,3));
+
+
+            Console.WriteLine(Beregn(1; 2, 4,5));
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+            
+        }
+        public static int Beregn(int a, int b)
+        {
+            return a + b;
 
         }
+        public static int Beregn(int a, int b, int c)
+        {
+            return Beregn(a, b) + c;
+
+        }
+        public static int Beregn(int a, int b, int c, int d)
+        {
+
+            return Beregn(a, b, c) + d;
+
+        }
+
     }
+  
+    
 }
