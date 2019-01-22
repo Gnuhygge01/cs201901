139e9db08d41185b666766a25d90254d7fff20a4
admin <admin@TA-828-03>
2019-01-22 11:35:07 +0100

    leger med propfull

---
+++ Komplette egenskaber/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komplette_egenskaber
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vare v = new Vare();
+            v.Navn = "vare #1";
+            v.Pris = 100;
+            Console.WriteLine(v.PrisMedMoms());
+            Vare v2 = new Vare("vare #2", 200);
+            Console.WriteLine(v2.PrisMedMoms());
+
+            Vare v1 = new Vare();
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+
+        public class Vare
+        {
+            public Vare()
+            {
+
+            }
+            public Vare(string navn, int pris)
+            {
+                this.Pris = pris;
+                this.Navn = navn;
+            }
+
+            private string  navn;
+
+            public string  Navn
+            {
+                get {
+
+                    return navn;
+                }
+                set {
+
+                    navn = value;
+                }
+            }
+            public double Pris;
+
+
+
+        }
+
+
+
+
+
+        
+    }
+}
+
