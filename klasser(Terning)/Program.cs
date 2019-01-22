98a98ab0d5954db0485c9d479e7712329b25ace9
admin <admin@TA-828-03>
2019-01-22 10:03:52 +0100

    terninger

---
+++ klasser(Terning)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_Terning_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t1 = new Terning();
+            t1.Skriv();
+            t1.Ryst();
+            t1.Skriv();
+
+            Terning t2 = new Terning(true);
+            t2.Skriv();
+            t2.Ryst();
+            t2.Skriv();
+
+            Terning t3 = new Terning(true);
+            t3.Skriv();
+            t3.Ryst();
+            t3.Skriv();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+    public class Terning
+    {
+        private static Random rnd = new Random();
+        public int værdi;
+        private bool snyd;
+
+        public Terning()
+        {
+            this.værdi = 1;
+            this.snyd = false;
+        }
+
+        public Terning(bool snyd)
+        {
+            this.snyd = snyd;
+            Ryst();
+        }
+        public void Skriv()
+        {
+            Console.WriteLine("[" + this.værdi + "]");
+        }
+
+        public void Ryst()
+        {
+            if (snyd)
+                this.værdi = 6;
+            else
+                this.værdi = rnd.Next(1, 7);
+        }
+
+    }
+
+
+
+}
