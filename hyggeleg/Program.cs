671403098df6e5c831d6fc19a0d2d8b4d7a8b15a
admin <admin@TA-828-03>
2019-01-22 17:04:35 +0100

    tilføjet hyggeleg int

--- hyggeleg/Program.cs
+++ hyggeleg/Program.cs
@@ -10,7 +10,34 @@ namespace hyggeleg
     {
         static void Main(string[] args)
         {
+            int res = Beregn.Lægsammen(65, 34);
+
+            Console.WriteLine(res);
+            int res2 = Beregn.Trækfra(45, 67);
+            Console.WriteLine(res2);
+
+            Console.ReadKey();
+
         }
+         
+        public class Beregn
+        {
+            public static int Lægsammen(int a, int b)
+            {
+                return a + b;
+            }
+            public static int Trækfra(int a, int b)
+            {
+
+                return a - b;
+
+            }
+            private Beregn() { }
+        }
+        
+        
+
+
 
     }
 
