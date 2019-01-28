799e001cd7da544ba58d0fea7020e79bc0b1a241
admin <admin@TA-8214-02>
2019-01-28 11:34:44 +0100

    arvet

---
+++ Arv Exception/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StockItem s = new StockItem(-1);
+
+            }
+            catch (StockItemException ex)
+            {
+                Console.WriteLine("StockItem Error : " + ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General Error");
+            }
+
+
+        }
+    }
+    public class StockItem
+    {
+        public int Id { get; set; }
+
+        public StockItem (int id)
+        {
+            if (id < 0)
+                throw new StockItemException("forkert id"); 
+        }
+
+    }
+    public class StockItemException : Exception
+    {
+        public StockItemException(string message) : base(message)
+        { }
+
+    }
+    
+}
