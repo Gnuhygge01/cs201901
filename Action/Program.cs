1d5c9795bca34aec70f983a685b835c2d0bacc21
admin <admin@TA-8214-02>
2019-01-29 13:48:49 +0100

    action

---
+++ Action/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action
+{
+     
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Action<String> f1 = (MinSkrivTilConsole);
+            f1.Invoke("test");
+
+            Action<String> f2 = MinSkrivTilConsole;
+            f2.DynamicInvoke("test");
+
+            Action<string> f3 = MinSkrivTilConsole;
+            f3 += Console.WriteLine;
+            f3("Gnuen er");
+
+            f1 -= MinSkrivTilConsole;
+            f2 -= MinSkrivTilConsole;
+            f3 -= MinSkrivTilConsole;
+            f3 -= Console.WriteLine;
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+
+        static void MinSkrivTilConsole(string txt)
+        {
+            Console.WriteLine("**** " + txt + " Gnuen");
+        }
+    }
+}
