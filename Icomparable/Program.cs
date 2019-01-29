790d9aeac75a4d03f679907438229b354a664780
admin <admin@TA-8214-02>
2019-01-29 12:12:10 +0100

    gyd

--- Icomparable/Program.cs
+++ Icomparable/Program.cs
@@ -10,16 +10,16 @@ namespace Icomparable
     {
         static void Main(string[] args)
         {
-            Hund[] hunde = new Hund[4];
+            Hund[] hunde = new Hund[5];
             hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
             hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
             hunde[2] = new Hund() { Alder = 7, Navn = "Bonde" };
-            hunde[3] = new Hund() { Alder = 4, Navn = "gydeas" };
-
+            hunde[3] = new Hund() { Alder = 8 , Navn = "gydeas" };
+            hunde[4] = new Hund() { Alder = 2, Navn = "gydeas" };
             Array.Sort(hunde);
             foreach (var item in hunde)
             {
-                Console.WriteLine(item.Alder + item.Navn);
+                Console.WriteLine(item.Alder +" " + item.Navn);
 
             }
             if (System.Diagnostics.Debugger.IsAttached)
