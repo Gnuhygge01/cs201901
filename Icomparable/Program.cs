b60c9e0b6943f8612ccd1999f45d59e227de0f87
admin <admin@TA-8214-02>
2019-01-29 11:02:25 +0100

    icom

---
+++ Icomparable/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icomparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[4];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            hunde[2] = new Hund() { Alder = 7, Navn = "Bonde" };
+            hunde[3] = new Hund() { Alder = 4, Navn = "gydeas" };
+
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Alder + item.Navn);
+
+            }
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Hund :IComparable <Hund>
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+
+        public int CompareTo(Hund h)
+
+
+        {
+            if (h.Alder > this.Alder)
+                return -1;
+            if (h.Alder < this.Alder)
+                return 1;
+            return 0;
+
+        }
+    }
+
+
+}
---
+++ sololearn/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sololearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 15; x++)
+            {
+                Console.WriteLine("værdi af x : {0}",x);
+            }
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
